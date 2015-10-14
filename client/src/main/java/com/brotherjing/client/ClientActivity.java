2d466e627c720e85c3604c0e93269c97273b2eb6
brotherjing <brotherjing@sjtu.edu.cn>
2015-10-14 22:56:54 +0800

    vuforia and baidu asr

--- client/src/main/java/com/brotherjing/client/ClientActivity.java
+++ client/src/main/java/com/brotherjing/client/ClientActivity.java
@@ -26,6 +26,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.baidu.voicerecognition.android.ui.BaiduASRDigitalDialog;
+import com.brotherjing.client.ImageTargets.ImageTargets;
 import com.brotherjing.client.service.TCPClient;
 import com.brotherjing.utils.ImageCache;
 import com.brotherjing.utils.bean.TCPMessage;
@@ -58,7 +59,7 @@ public class ClientActivity extends ActionBarActivity {
     private EditText edt_ip, edt_port;
     private Button btn_connect;
     private EditText edt_input;
-    private Button btn_submit,btn_asr;
+    private Button btn_submit,btn_asr,btn_ar;
     private LinearLayout ll_chat;
 
     @Override
@@ -79,6 +80,8 @@ public class ClientActivity extends ActionBarActivity {
         edt_input = (EditText) findViewById(R.id.edt_input);
         btn_submit = (Button) findViewById(R.id.btn_submit);
         btn_asr = (Button) findViewById(R.id.btn_asr);
+        btn_ar = (Button) findViewById(R.id.btn_ar);
+
         ll_chat = (LinearLayout) findViewById(R.id.ll_chat);
 
         btn_connect.setOnClickListener(new View.OnClickListener() {
@@ -100,24 +103,13 @@ public class ClientActivity extends ActionBarActivity {
         btn_asr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*Intent intent = new Intent(ClientActivity.this,BaiduASRDigitalDialog.class);
-                //intent.setAction("com.baidu.action.RECOGNIZE_SPEECH");
-                intent.putExtra("appid", CONSTANT.APP_ID);
-                intent.putExtra("key", CONSTANT.API_KEY);
-                intent.putExtra("secret", CONSTANT.SECRET_KEY);
-                intent.putExtra("sound_start", R.raw.bdspeech_recognition_start);
-                intent.putExtra("sound_end", R.raw.bdspeech_speech_end);
-                intent.putExtra("sound_success", R.raw.bdspeech_recognition_success);
-                intent.putExtra("sound_error", R.raw.bdspeech_recognition_error);
-                intent.putExtra("sound_cancel", R.raw.bdspeech_recognition_cancel);
-                intent.putExtra("sample", 16000);
-
-                intent.putExtra("language", "cmn-Hans-CN");
-                //Log.i("yj", Environment.getExternalStorageDirectory().getAbsolutePath()+"/temp_license_2015-10-14");
-                intent.putExtra("license-file-path", "/sdcard/temp_license_2015-10-14");
-                startActivityForResult(intent, REQUEST_UI);*/
-                BaiduVoiceHelper.startBaiduVoiceDialogForResult(ClientActivity.this,
-                        CONSTANT.API_KEY, CONSTANT.SECRET_KEY, REQUEST_UI);
+                BaiduVoiceHelper.startBaiduVoiceDialogForResult(ClientActivity.this, CONSTANT.API_KEY, CONSTANT.SECRET_KEY, REQUEST_UI);
+            }
+        });
+        btn_ar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ClientActivity.this, ImageTargets.class));
             }
         });
     }
---
+++ client/src/main/java/com/brotherjing/client/SampleApplication/utils/MyObject.java
@@ -0,0 +1,91 @@
+package com.brotherjing.client.SampleApplication.utils;
+
+import java.nio.Buffer;
+
+/**
+ * Created by Brotherjing on 2015/10/4.
+ */
+public class MyObject extends MeshObject{
+
+    private Buffer mVertBuff;
+    private Buffer mTexCoordBuff;
+    private Buffer mNormBuff;
+    private Buffer mIndBuff;
+
+    private int indicesNumber = 0;
+    private int verticesNumber = 0;
+
+
+    public MyObject()
+    {
+        setVerts();
+        setTexCoords();
+        setNorms();
+        setIndices();
+    }
+
+    private void setVerts(){
+        float myVertices[] = new float[]{
+                -0.5f,-0.5f,0.0f,
+                0.5f,-0.5f,0.0f,
+                0.5f,0.5f,0.0f,
+                -0.5f,0.5f,0.0f
+        };
+        mVertBuff = fillBuffer(myVertices);
+        verticesNumber = myVertices.length/3;
+    }
+
+    private void setTexCoords(){
+        float[] myTexs = new float[]{
+                0.0f,0.0f,
+                1.0f,0.0f,
+                1.0f,1.0f,
+                0.0f,1.0f
+        };
+        mTexCoordBuff = fillBuffer(myTexs);
+    }
+
+    private void setNorms(){
+        float myNorms[] = new float[]{
+                0.0f,0.0f,1.0f,
+                0.0f,0.0f,1.0f,
+                0.0f,0.0f,1.0f,
+                0.0f,0.0f,1.0f
+        };
+        mNormBuff = fillBuffer(myNorms);
+    }
+
+    private void setIndices(){
+        short myIndices[] = new short[]{
+                0,1,2,
+                2,3,0
+        };
+        mIndBuff = fillBuffer(myIndices);
+        indicesNumber = myIndices.length;
+    }
+
+    @Override
+    public Buffer getBuffer(BUFFER_TYPE bufferType) {
+        switch (bufferType){
+            case BUFFER_TYPE_VERTEX:
+                return mVertBuff;
+            case BUFFER_TYPE_TEXTURE_COORD:
+                return mTexCoordBuff;
+            case BUFFER_TYPE_NORMALS:
+                return mNormBuff;
+            case BUFFER_TYPE_INDICES:
+                return mIndBuff;
+            default:return null;
+        }
+    }
+
+    @Override
+    public int getNumObjectVertex() {
+        return verticesNumber;
+    }
+
+    @Override
+    public int getNumObjectIndex() {
+        return indicesNumber;
+    }
+}
